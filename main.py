cb807aa7e57c97aa1ffe55a09d57a8267dbc1daf
caixiang <309611794@qq.com>
2019-12-01 14:22:23 +0800

    [add]add eat food

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 # -*-coding:utf-8-*-
 
 import web
-
-def main():
-    sex = input("input your sex(F/M):")
-    weight = float(input("input your weight(kg):"))
-    height = float(input("input your height(cm):"))
-    age = float(input("input your age(year):"))
-    if sex == "M":
-        heat = 66 + 13.7 * weight + 5 * height - 6.8 * age
-    else:
-        heat = 655 + 9.6 * weight + 1.8 * height - 4.7 * age
-    print ("your base heat is %s" % heat)
+import requests
 
 urls = ("/", "index",
         "/add", "add")
 render = web.template.render("templates/")
+
+def request_get(word):
+    key = "d99936156f33eb2a11ba6545ba7182b4"
+    url = "http://api.tianapi.com/txapi/nutrient/index?key=%s&word=%s&mode=0" % (key, word)
+    ret = requests.get(url=url)
+    content = eval(ret.content)
+    if content.get("code") == 200:
+        return content.get("newslist")[0].get("rl")
 
 class index():
     def GET(self):
@@ -31,11 +29,19 @@ class index():
             heat = 66 + 13.7 * weight + 5 * height - 6.8 * age
         else:
             heat = 655 + 9.6 * weight + 1.8 * height - 4.7 * age
-        return "your base heat is %s" % heat
+        allheat = heat
+        for i in range(1, 12):
+            food_name = "food" + str(i)
+            food = data.get(food_name)
+            food_weight = food_name + "_weight"
+            foodWeight = data.get(food_weight)
+            if food and foodWeight:
+                createHeat = float(request_get(food)) * float(foodWeight) / 100
+                allheat = allheat - createHeat
 
-class add():
-    def GET(self):
-        return "hello,world!"
+        return "your base heat is %s" % -allheat
+
+
 
 
 if __name__ == "__main__":
